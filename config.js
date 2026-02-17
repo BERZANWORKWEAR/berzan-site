8afb654d4fbb9ac30c7ae3ca30a0b19a8f839db4
BERZANWORKWEAR <berzan@berzan.com.tr>
2026-02-17 03:30:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,6 +12,6 @@ window.BERZAN_CFG = window.BERZAN_CFG || {
 // Supabase (opsiyonel)
 // Burayı kendi Supabase bilgilerinle doldur (Project Settings > API)
 window.__SUPABASE__ = window.__SUPABASE__ || {
-  url: 'PASTE_SUPABASE_URL',
-  anonKey: 'PASTE_SUPABASE_ANON_KEY',
+  url: "https://vfncwzwuelykytdoedqd.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmbmN3end1ZWx5a3l0ZG9lZHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNzI0NDMsImV4cCI6MjA4NjY0ODQ0M30.tCQaCAaSdUbx6rFUFStrrH1Sb3kS5P5IBgR-kdBBB6c",
 };
